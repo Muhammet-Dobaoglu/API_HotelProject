86bd800aa0ee8617d4d2c766b1765241d968d89a
Muhammet-Dobaoglu <muhammetdobaoglu@hotmail.com>
2023-03-25 21:09:09 +0300

    RapidApi

--- Ramada_Hotel_WebUI/Controllers/RegisterController.cs
+++ Ramada_Hotel_WebUI/Controllers/RegisterController.cs
@@ -1,6 +1,46 @@
-﻿namespace Ramada_Hotel_WebUI.Controllers
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Ramada_Hotel_WebUI.Dtos.RegisterDto;
+using System.Threading.Tasks;
+
+namespace Ramada_Hotel_WebUI.Controllers
 {
-    public class RegisterController
+    [AllowAnonymous]
+    public class RegisterController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        { 
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateNewUserDto createNewUserDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            var appUser = new AppUser()
+            {
+                Name = createNewUserDto.Name,
+                Email = createNewUserDto.Mail,
+                Surname = createNewUserDto.Surname,
+                UserName = createNewUserDto.Username,
+            };
+            var result = await _userManager.CreateAsync(appUser, createNewUserDto.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            return View();
+        }
     }
 }
--- Ramada_Hotel_WebUI/Mapping/AutoMapperConfig.cs
+++ Ramada_Hotel_WebUI/Mapping/AutoMapperConfig.cs
@@ -20,11 +20,11 @@ namespace Ramada_Hotel_WebUI.Mapping
             CreateMap<UpdateServiceDto, Service>().ReverseMap();
             CreateMap<CreateServiceDto, Service>().ReverseMap();
 
-            //CreateMap<CreateNewUserDto, AppUser>().ReverseMap();
-            //CreateMap<LoginUserDto, AppUser>().ReverseMap();
+            CreateMap<CreateNewUserDto, AppUser>().ReverseMap();
+            CreateMap<LoginUserDto, AppUser>().ReverseMap();
 
-            //CreateMap<ResultAboutDto, About>().ReverseMap();
-            //CreateMap<UpdateAboutDto, About>().ReverseMap();
+            CreateMap<ResultAboutDto, About>().ReverseMap();
+            CreateMap<UpdateAboutDto, About>().ReverseMap();
 
             CreateMap<ResultTestimonialDto, Testimonial>().ReverseMap();
 
@@ -32,11 +32,11 @@ namespace Ramada_Hotel_WebUI.Mapping
 
             CreateMap<CreateSubscribeDto, Subscribe>().ReverseMap();
 
-            //CreateMap<CreateBookingDto, Booking>().ReverseMap();
-            //CreateMap<ApprovedReservationDto, Booking>().ReverseMap();
+            CreateMap<CreateBookingDto, Booking>().ReverseMap();
+            CreateMap<ApprovedReservationDto, Booking>().ReverseMap();
 
-            //CreateMap<CreateGuestDto, Guest>().ReverseMap();
-            //CreateMap<UpdateGuestDto, Guest>().ReverseMap();
+            CreateMap<CreateGuestDto, Guest>().ReverseMap();
+            CreateMap<UpdateGuestDto, Guest>().ReverseMap();
         }
     }
 }

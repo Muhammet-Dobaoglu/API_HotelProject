d4fcd35fc5c0d5116bd2e653130e4f18411d4e16
Muhammet-Dobaoglu <muhammetdobaoglu@hotmail.com>
2023-03-26 12:24:47 +0300

    Controller

---
+++ ApiConsume/Ramada_Hotel_WebApi/Controllers/BookingController.cs
@@ -0,0 +1,62 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ramada_Hotel_WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+        private readonly IBookingService _bookingService;
+        public BookingController(IBookingService bookingService)
+        {
+            _bookingService = bookingService;
+        }
+
+        [HttpGet]
+        public IActionResult BookingList()
+        {
+            var values = _bookingService.TGetList();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult AddBooking(Booking booking)
+        {
+            _bookingService.TInsert(booking);
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBooking(int id)
+        {
+            var values = _bookingService.TGetByID(id);
+            _bookingService.TDelete(values);
+            return Ok();
+        }
+        [HttpPut("UpdateBooking")]
+        public IActionResult UpdateBooking(Booking booking)
+        {
+            _bookingService.TUpdate(booking);
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetBooking(int id)
+        {
+            var values = _bookingService.TGetByID(id);
+            return Ok(values);
+        }
+        //[HttpPut("aaaaa")]
+        //public IActionResult aaaaa(Booking booking)
+        //{
+        //    _bookingService.TBookingStatusChangeApproved(booking);
+        //    return Ok();
+        //}
+        //[HttpPut("bbbb")]
+        //public IActionResult bbbb(int id)
+        //{
+        //    _bookingService.TBookingStatusChangeApproved2(id);
+        //    return Ok();
+        //}
+    }
+}
---
+++ Ramada_Hotel_WebUI/Controllers/RoomController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ramada_Hotel_WebUI.Controllers
+{
+    public class RoomController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
